20561b825062e15d48bc7d0b2102a22abdea59be
dudcks5477 <rladudcks5477@naver.com>
2025-01-05 14:52:08 +0900

    Delete user Memory Calender

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -8,7 +8,7 @@ import profileScreenStyle from '../styles/profileScreenStyle';
 import HeaderProfile from '../components/HeaderProfile';
 import IntroSection from '../components/IntroSection';
 import UserNameShare from '../components/UserNameShare';
-import UserMemorizeSection from '../components/UserMemorizeSection/UserMemorizeSection';
+// import UserMemorizeSection from '../components/UserMemorizeSection/UserMemorizeSection';
 
 const images = [
   require('../assets/random/mountain.jpg'),
@@ -27,12 +27,8 @@ const getRandomImage = () => {
 const ProfileScreen = () => {
   const navigation = useNavigation();
   const {userData} = useContext(UserContext);
-  const [isPlaying, setIsPlaying] = useState(false);
+  // const [isPlaying, setIsPlaying] = useState(false);
   const [randomImage, setRandomImage] = useState(getRandomImage());
-  // const [nickname, setNickname] = useState('zerochan');
-  // 추 후 국가 추가시 변경
-  // const [country, setCountry] = useState('KOREA');
-  // 가입 연도 상태 추가 (예시 0년 설정)
   const [joinYear, setJoinYear] = useState(0);
 
   useEffect(() => {
@@ -51,35 +47,34 @@ const ProfileScreen = () => {
     navigation.navigate('EditProfile');
   };
 
-  const togglePlayPause = () => {
-    setIsPlaying(!isPlaying);
-  };
+  // const togglePlayPause = () => {
+  //   setIsPlaying(!isPlaying);
+  // };
 
-  useEffect(() => {
-    if (isPlaying) {
-      const timer = setTimeout(() => {
-        setIsPlaying(false);
-      }, 3000);
+  // useEffect(() => {
+  //   if (isPlaying) {
+  //     const timer = setTimeout(() => {
+  //       setIsPlaying(false);
+  //     }, 3000);
 
-      return () => clearTimeout(timer);
-    }
-  }, [isPlaying]);
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, [isPlaying]);
 
   return (
     <View style={Common.container}>
       <Image source={randomImage} style={profileScreenStyle.image} />
       <HeaderProfile
-        // 로직 고민 필요 (첫 가입자랑 기존 가입자 분리)
         handleUploadMain={handleUploadMain}
         handleEditProfile={handleEditProfile}
         nickname={userData.nickName}
       />
       <UserNameShare nickname={userData.nickName} />
       <IntroSection country={userData.countryName} joinYear={joinYear} />
-      <UserMemorizeSection
+      {/* <UserMemorizeSection
         isPlaying={isPlaying}
         togglePlayPause={togglePlayPause}
-      />
+      /> */}
     </View>
   );
 };
